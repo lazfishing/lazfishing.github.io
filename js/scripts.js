6a0e4a7a51cb4d8d58fcd29b6909f7c35dcbd1da
lazfishing <54772874+lazfishing@users.noreply.github.com>
2021-02-05 08:25:00 +0800

    migrated code from jsfiddle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,34 @@
 console.log('Hello, looks like the javascript file is loading properly')
+
+function islington() {
+	$('#islington').show();
+  $('#holborn').hide()
+  $('#king').hide()
+}
+
+function king() {
+	$('#islington').hide();
+  $('#holborn').hide()
+  $('#king').show()
+}
+
+function holborn() {
+	$('#islington').hide();
+  $('#holborn').show()
+  $('#king').hide()
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
